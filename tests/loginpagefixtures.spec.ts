58ce57663c810b3adf50033554156f9881ca3f25
Sravani <sravanibhumireddy2407@gmail.com>
2026-08-31 12:17:59 +0530

    Update login test for PR CI

--- tests/loginpagefixtures.spec.ts
+++ tests/loginpagefixtures.spec.ts
@@ -7,6 +7,7 @@ test.beforeEach(async({loginPage})=>{
 
 
 test('@smoke user is able to login',async({loginPage,homePage})=>{
+    console.log("entering user credentials");
 await loginPage.doLogin(process.env.APP_USERNAME!,process.env.APP_PASSWORD!);
 expect.soft (await homePage.isLogoutLinkExist()).toBeTruthy();
 expect.soft(await homePage.getHomePageTitle()).toBe('My Account')
